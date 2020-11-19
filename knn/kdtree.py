4c0a625fda7d1c974d9552cf68fe569aecc5b8e2
ShataKurashi <soccer.271828.339@gmail.com>
2020-11-19 22:55:18 +0900

    Modified document comment

--- knn/kdtree.py
+++ knn/kdtree.py
@@ -6,26 +6,29 @@
     scikit-learn (https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KDTree.html)
     C言語によるkd-tree の実装 (https://qiita.com/fj-th/items/1bb2dc39f3088549ad6e)
     機械学習の2、kd-tree最近傍探索 (https://memo.soarcloud.com/%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92%E3%81%AE%EF%BC%92%E3%80%81kd-tree%E3%81%AB%E3%82%88%E3%82%8B%E6%9C%80%E8%BF%91%E5%82%8D%E6%8E%A2%E7%B4%A2/)
+    Kd-treeを実装してみた (https://atkg.hatenablog.com/entry/2016/12/18/002353)
 
 """
 
 import numpy as np
 from matplotlib import pyplot as plt
+from abc import ABC
 import warnings
 
-""" 
-" Node class
-" This class represents a node of a binary tree.
-"
-" Attributes:
-    x       (float, ndarray): Data.
-    depth   (int)           : Depth of this node. The root is 0.
-    is_leaf (bool)          : is_leaf is true if this node is true.
-    left    (Node, NoneType): Left node.
-    right   (Node, NoneType): Right node.
-    parent  (Node, NoneType): Parent node.
-"""
 class Node:
+    """ 
+    " Node class
+    " This class represents a node of a binary tree.
+    "
+    " Attributes:
+        x       (float, ndarray): Data.
+        depth   (int)           : Depth of this node. The root is 0.
+        is_leaf (bool)          : is_leaf is true if this node is true.
+        left    (Node, NoneType): Left node.
+        right   (Node, NoneType): Right node.
+        parent  (Node, NoneType): Parent node.
+    """
+
     def __init__ (self, x, depth, is_leaf=False, left=None, right=None, parent=None):
         self.x = x
         self.depth = depth
@@ -34,25 +37,27 @@ class Node:
         self.right = right
         self.parent = parent
 
-    def print_tree (self):
+    def print_node (self):
         if not self.is_leaf:
-            self.left.print_tree ()
+            self.left.print_node ()
         for i in range (self.depth):
             print ("+", end="")
         print (self.x)
         if not self.is_leaf and self.right!=None:
-            self.right.print_tree ()
+            self.right.print_node ()
 
-"""
-" This class represents KD-Tree
-" 
-" TODO:
-    Create binary tree class as a super class
-"
-" Attribute:
-    tree (Node) : The parent node of kd tree
-"""
+
 class KDTree:
+    """
+    " This class represents KD-Tree
+    " 
+    " TODO:
+        Create binary tree class as a super class
+    "
+    " Attribute:
+        tree (Node) : The parent node of kd tree
+    """
+
     def __init__  (self, data):
         self.tree = self.recursive_build (data, 0)
 
@@ -66,6 +71,7 @@ class KDTree:
             return None
 
         if n==1:
+            # Leaf node
             return Node (data[0], depth, True)
 
         split_axis = depth % dim
@@ -82,9 +88,29 @@ class KDTree:
 
         return node
 
+    def query (self, data, k=1):
+        
+
+    # print
+    def print_tree (self):
+        self.tree.print_node ()
+
     # Visualize
     def plot_2d_tree (self, x_range, y_range):
         _plot_2d_tree_recursive (self.tree, x_range, y_range)
+
+
+class Distance:
+    @classmethod
+    def distance (p1, p2):
+        """
+        Euclidian distance
+
+        TODO:
+            Create abstract distance class
+            and create concrete (Euclidean or other) distance class
+        """
+        return np.sum ((p1-p2)**2)
 
 # Subroutine method to recursive
 def _plot_2d_tree_recursive (node, x_range, y_range, ax=None):
@@ -131,6 +157,6 @@ def _plot_2d_tree_recursive (node, x_range, y_range, ax=None):
 if __name__ == '__main__':
     data = np.random.rand (8,2) * 2 - 1
     tree = KDTree (data)
-    tree.tree.print_tree ()
+    tree.tree.print_node ()
     tree.plot_2d_tree ([-1,1],[-1,1])
     plt.show()
